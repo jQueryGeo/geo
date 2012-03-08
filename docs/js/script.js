7aa38f23ee3c17ee25a57d99546337cd072e55eb
Ryan <ryan@trippingthebits.com>
2012-03-08 18:18:33 -0500

    nothing special in the docs for IE

--- docs/js/script.js
+++ docs/js/script.js
@@ -2,17 +2,7 @@
 
 */
 
-if ($("html").hasClass("ie9") || $("html").hasClass("ie8")) {
-  $(".ui-page").live("pageshow", function (e) {
-    window.focus();
-  });
-}
-
 $(function () {
-  $(document).bind("mobileinit", function () {
-    $.mobile.gradeA = function () { return $.support.mediaquery || $("html").hasClass("ie8") };
-  });
-
   $("a[data-href]").live("click", function (e) {
     $("#" + $(this).data("href"))[0].scrollIntoView();
   });
