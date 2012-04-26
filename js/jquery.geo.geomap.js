08ab19c1e6e1971062458fc58930588269b40fea
TrippingTheBits <ryan@trippingthebits.com>
2012-04-26 14:48:42 -0400

    geomap - [bug] measure text can sometimes get stuck in top-left corner

--- js/jquery.geo.geomap.js
+++ js/jquery.geo.geomap.js
@@ -760,7 +760,7 @@
       this._$contentFrame.append('<div class="geo-draw-container" style="' + contentPosCss + contentSizeCss + '"></div>');
       this._$drawContainer = this._$contentFrame.children(':last');
 
-      this._$contentFrame.append('<div class="geo-measure-container" style="' + contentPosCss + contentSizeCss + '"><div class="geo-measure-label" style="' + contentPosCss + '; display: none;"></div></div>');
+      this._$contentFrame.append('<div class="geo-measure-container" style="' + contentPosCss + contentSizeCss + '"><span class="geo-measure-label" style="' + contentPosCss + '; display: none;"></span></div>');
       this._$measureContainer = this._$contentFrame.children(':last');
       this._$measureLabel = this._$measureContainer.children();
 
