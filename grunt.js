9803ffa70f237d6e7b664d6288535772f6fdb07a
Ryan Westphal <ryan@trippingthebits.com>
2013-05-09 18:36:35 -0400

    remove download property from package json files

--- grunt.js
+++ grunt.js
@@ -25,7 +25,7 @@ module.exports = function(grunt) {
           'js/jquery.geo.tiled.js',
           'js/jquery.geo.shingled.js'
         ],
-        dest: 'docs/<%= pkg.name %>-<%= pkg.version %>.js'
+        dest: 'dist/<%= pkg.name %>-<%= pkg.version %>.js'
       }
     },
     min: {
@@ -43,7 +43,7 @@ module.exports = function(grunt) {
       },
       dist: {
         src: ['<banner>', '<config:concat.dist.dest>'],
-        dest: 'docs/<%= pkg.name %>-<%= pkg.version %>.min.js'
+        dest: 'dist/<%= pkg.name %>-<%= pkg.version %>.min.js'
       }
     },
     qunit: {
