d6568c654bfe14c3289b40ad5417453437a71364
Ryan Westphal <ryan@trippingthebits.com>
2014-05-11 21:30:48 -0400

    update jquery & jquery geo refs, remove jquery mobile from docs

--- docs/js/script.js
+++ docs/js/script.js
@@ -3,17 +3,6 @@
 */
 
 $(function () {
-  $("a[data-href]").live("click", function (e) {
-    $("#" + $(this).data("href"))[0].scrollIntoView();
-  });
-
-  $(".ui-page").live("pageshow", function () {
-    $(this).find(".geomap-indoc").geomap({ zoom: 2, scroll: "off" });
-  });
-
-  $(".ui-page").live("pagebeforehide", function () {
-    $(this).find(".geomap-indoc").geomap("destroy");
-  });
 });
 
 
