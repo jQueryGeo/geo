9b7807448ffed4a7666db1baae27937909bf9a37
Ryan Morrison~Westphal <ryan@trippingthebits.com>
2015-04-24 11:29:40 -0400

    #178 - something's wrong with find tests, append cancels tests

--- test/qunit/find.js
+++ test/qunit/find.js
@@ -1,13 +1,59 @@
-QUnit.test( "find", function( assert ) {
-  $('#map').geomap();
+QUnit.test( 'nothingToFind', function( assert ) {
+  var map = $('#map').geomap();
 
-  assert.equal( $( '.geo-service' ).length, 1, "map created successfully!" );
+  var geo = map.geomap( 'find', {
+    type: 'Point',
+    coordinates: [ -71, 40 ]
+  }, 1 );
 
+  assert.equal( geo.length, 0, "no features found (non were available)!" );
 });
 
-QUnit.test( "find2", function( assert ) {
-  $('#map').geomap();
+QUnit.asyncTest( "basicFind", function( assert ) {
+  var map = $('#map').geomap();
 
-  assert.equal( $( '.geo-service' ).length, 1, "map created successfully!" );
+  setTimeout( function() {
+    map.geomap( 'append', {
+      type: 'Point',
+      coordinates: [ -71, 40 ]
+    } );
+
+    var geo = map.geomap( 'find', {
+      type: 'Point',
+      coordinates: [ -71, 40 ]
+    }, 1 );
+
+    assert.equal( geo.length, 1, "Point found!" );
+  }, 32 );
 });
 
+QUnit.test( 'alwaysPass', function( assert ) {
+  assert.equal( '1', 1, "always pass!" );
+});
+
+
+/*
+QUnit.test( "findTopFirst", function( assert ) {
+  // the last shape to be appended,
+  // should be the first to be found
+  var map = $('#map').geomap();
+
+  map.geomap( 'append', {
+    type: 'Feature',
+    geometry: {
+      type: 'Point',
+      coordinates: [ -71, 40 ]
+    },
+    properties: {
+      name: '1'
+    }
+  } );
+
+  var geo = map.geomap( 'find', {
+    type: 'Point',
+    coordinates: [ -71, 40 ]
+  }, 1 );
+
+  assert.equal( geo.length, 1, "Another Point found!" );
+});
+*/
